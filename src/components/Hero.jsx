4477a8fb55c0b9f4f43c2a250b07ac1cd38e9d22
Yisaaaa <ryansanisit19@gmail.com>
2023-10-01 16:50:26 +0800

    done doing Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import heroImg from "../hero.svg";
+
+export default function Hero() {
+  return (
+    <section className="hero container">
+      <div className="hero-img-container">
+        <img src={heroImg} className="hero-img" />
+      </div>
+      <div className="hero-text-box">
+        <h1 className="primary--heading">Online Experiences</h1>
+        <p className="hero-text">
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </section>
+  );
+}
