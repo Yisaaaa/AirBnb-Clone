5135f25f52915c19c2a6f069b7539c66c65deed9
Yisaaaa <ryansanisit19@gmail.com>
2023-10-03 17:50:48 +0800

    made Card component work and rendered it on the page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Header";
 import Hero from "./Hero";
+import Card from "./Card";
+import Data from "../data";
 
 function App() {
+  const cards = Data.map((card) => {
+    return (
+      <Card
+        img={card.coverImg}
+        rating={card.stats.rating}
+        reviewCount={card.stats.reviewCount}
+        location={card.location}
+        title={card.title}
+        price={card.price}
+      />
+    );
+  });
+
   return (
     <>
       <Header />
       <Hero />
+      <div className="cards">{cards}</div>
     </>
   );
 }
